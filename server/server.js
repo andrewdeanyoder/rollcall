c4e2029fd1ed16d9ff58f194c06e73cf19d24149
andrewdeanyoder <andrew.dean.yoder@gmail.com>
2020-09-13 22:10:25 -0700

    updated api endpoint to return dummy data

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const path = require('path');
 const app = express()
 const port = 3000
 
+const dataFilePath = '../database/dataImages.json';
+
 app.use((req, res, next) => {
   console.log(`serving ${req.method} for ${req.url}`);
   next();
@@ -11,8 +13,10 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use('/', express.static(path.join(__dirname, '../public')));
 
+// TODO: replace this with a database
 app.get('/api/data', (req, res) => {
-  res.status(200).send('Hello World!\n');
+  const dataFile = require(dataFilePath);
+  res.status(200).send(dataFile);
 })
 
 app.listen(port, () => {
